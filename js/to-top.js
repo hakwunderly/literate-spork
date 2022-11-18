cc169d36809ba364d516c974a2ddba6ad911bfcb
jo Wu <JWunderly@supremecourt.gov>
2022-11-18 17:05:32 -0500

    Change footer

--- js/to-top.js
+++ js/to-top.js
@@ -1,7 +1,7 @@
 const toTop = document.querySelector(".to-top");
 
 window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 100) {
+  if (window.pageYOffset > 25) {
     toTop.classList.add("active");
   } else {
     toTop.classList.remove("active");
